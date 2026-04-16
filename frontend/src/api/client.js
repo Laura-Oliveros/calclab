fe51219d8f958778a5213b1795d6da199a0546e3
Laura Oliveros <lau22_2023@outlook.es>
2026-04-15 21:39:25 -0500

    Corregir baseURL del frontend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://calclab-5jkj.onrender.com",});
+  baseURL: "https://calclab-5jkj.onrender.com/api",
+});
 
 export default api;
